35df91fa8316bccc3b4d0cb1def625c1fe4ac727
yashas224 <71977329+yashas224@users.noreply.github.com>
2021-07-25 15:20:50 +0530

    update
    update

---
+++ spring-security-facebook-logn/src/main/java/com/example/spring/springsecurityfacebooklogn/SpringSecurityFacebookLognApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.springsecurityfacebooklogn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityFacebookLognApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityFacebookLognApplication.class, args);
+	}
+
+}
---
+++ spring-security-facebook-logn/src/test/java/com/example/spring/springsecurityfacebooklogn/SpringSecurityFacebookLognApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.springsecurityfacebooklogn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityFacebookLognApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
